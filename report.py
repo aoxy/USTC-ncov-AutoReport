f0fdbe4b2c80eae8f6275b6538df8e23fcf6be54
aoxy <axy613@qq.com>
2021-02-02 23:03:52 +0800

    修复https bug

--- report.py
+++ report.py
@@ -9,6 +9,7 @@ import sys
 import argparse
 from bs4 import BeautifulSoup
 
+
 class Report(object):
     def __init__(self, stuid, password, data_path):
         self.stuid = stuid
@@ -19,34 +20,35 @@ class Report(object):
         session = self.login()
         cookies = session.cookies
         data = session.get(
-            "http://weixine.ustc.edu.cn/2020").text
-        data = data.encode('ascii','ignore').decode('utf-8','ignore')
+            "https://weixine.ustc.edu.cn/2020").text
+        data = data.encode('ascii', 'ignore').decode('utf-8', 'ignore')
         soup = BeautifulSoup(data, 'html.parser')
         token = soup.find("input", {"name": "_token"})['value']
 
         with open(self.data_path, "r+") as f:
             data = f.read()
             data = json.loads(data)
-            data["_token"]=token
+            data["_token"] = token
 
         headers = {
             'authority': 'weixine.ustc.edu.cn',
-            'origin': 'http://weixine.ustc.edu.cn',
+            'origin': 'https://weixine.ustc.edu.cn',
             'upgrade-insecure-requests': '1',
             'content-type': 'application/x-www-form-urlencoded',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36',
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'referer': 'http://weixine.ustc.edu.cn/2020/',
+            'referer': 'https://weixine.ustc.edu.cn/2020/',
             'accept-language': 'zh-CN,zh;q=0.9',
             'Connection': 'close',
             'cookie': 'PHPSESSID=' + cookies.get("PHPSESSID") + ";XSRF-TOKEN=" + cookies.get("XSRF-TOKEN") + ";laravel_session="+cookies.get("laravel_session"),
         }
 
-        url = "http://weixine.ustc.edu.cn/2020/daliy_report"
+        url = "https://weixine.ustc.edu.cn/2020/daliy_report"
         session.post(url, data=data, headers=headers)
-        data = session.get("http://weixine.ustc.edu.cn/2020").text
+        data = session.get("https://weixine.ustc.edu.cn/2020").text
         soup = BeautifulSoup(data, 'html.parser')
-        pattern = re.compile("2020-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}")
+        pattern = re.compile(
+            "202[1-9]{1}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}")
         token = soup.find(
             "span", {"style": "position: relative; top: 5px; color: #666;"})
         flag = False
@@ -54,7 +56,8 @@ class Report(object):
             date = pattern.search(token.text).group()
             print("Latest report: " + date)
             date = date + " +0800"
-            reporttime = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S %z")
+            reporttime = datetime.datetime.strptime(
+                date, "%Y-%m-%d %H:%M:%S %z")
             timenow = datetime.datetime.now(pytz.timezone('Asia/Shanghai'))
             delta = timenow - reporttime
             print(delta.seconds)
@@ -67,10 +70,10 @@ class Report(object):
         return flag
 
     def login(self):
-        url = "https://passport.ustc.edu.cn/login?service=http%3A%2F%2Fweixine.ustc.edu.cn%2F2020%2Fcaslogin"
+        url = "https://passport.ustc.edu.cn/login?service=https%3A%2F%2Fweixine.ustc.edu.cn%2F2020%2Fcaslogin"
         data = {
             'model': 'uplogin.jsp',
-            'service': 'http://weixine.ustc.edu.cn/2020/caslogin',
+            'service': 'https://weixine.ustc.edu.cn/2020/caslogin',
             'username': self.stuid,
             'password': str(self.password),
         }
@@ -82,12 +85,15 @@ class Report(object):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='USTC nCov auto report script.')
-    parser.add_argument('data_path', help='path to your own data used for post method', type=str)
+    parser = argparse.ArgumentParser(
+        description='USTC nCov auto report script.')
+    parser.add_argument(
+        'data_path', help='path to your own data used for post method', type=str)
     parser.add_argument('stuid', help='your student number', type=str)
     parser.add_argument('password', help='your CAS password', type=str)
     args = parser.parse_args()
-    autorepoter = Report(stuid=args.stuid, password=args.password, data_path=args.data_path)
+    autorepoter = Report(
+        stuid=args.stuid, password=args.password, data_path=args.data_path)
     count = 3
     while count != 0:
         ret = autorepoter.report()
